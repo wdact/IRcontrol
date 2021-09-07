bf0682ad2faddcc658828064c80380bf61bab9b7
WDACT <WDACT@DESKTOP-TNGKFUF>
2021-09-07 17:53:17 +0400

    Добавьте файлы проекта.

---
+++ IRcontrol/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace IRcontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 35);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Включить/Выключить   TV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(518, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Тест работы ИК излучателя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Настраиваемые клавиши";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(30, 35);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(141, 60);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Запись";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(191, 35);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(141, 60);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Стоп";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(100, 103);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(159, 60);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Воспроизвести";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(30, 196);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(149, 35);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Кнопка 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(30, 238);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(149, 35);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Кнопка 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(30, 279);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(149, 35);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Кнопка 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(210, 196);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(149, 35);
+            this.radioButton4.TabIndex = 15;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Кнопка 4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton5.Location = new System.Drawing.Point(210, 238);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(149, 35);
+            this.radioButton5.TabIndex = 16;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Кнопка 5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton6.Location = new System.Drawing.Point(210, 279);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(149, 35);
+            this.radioButton6.TabIndex = 17;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Кнопка 6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 333);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Управление ИК датчиками";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ IRcontrol/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRcontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosed += Form1_FormClosed;
+            
+            try
+            {
+                port = new SerialPort("COM3");
+                port.RtsEnable = true;
+                port.DtrEnable = true;
+                port.DataReceived += Port_DataReceived;
+                port.Open();
+
+
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Оборудование не подключено!");
+                Close();
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (port != null && port.IsOpen)
+                port.Close();
+        }
+
+        List<string> allCommands = new List<string>();
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var port = (SerialPort)sender;
+            var data = port.ReadExisting();
+            switch(data.Trim())
+            {
+                case "Button1":
+                    radioButton1.BeginInvoke(new Action(()=>radioButton1.Checked = true));
+                    radioButton1_CheckedChanged(radioButton1, null);
+                    button8_Click(null, null);
+                    break;
+                case "Button2":
+                    radioButton2.BeginInvoke(new Action(() => radioButton2.Checked = true));
+                    radioButton1_CheckedChanged(radioButton2, null);
+                    button8_Click(null, null);
+                    break;
+                case "Button3":
+                    radioButton3.BeginInvoke(new Action(() => radioButton3.Checked = true));
+                    radioButton1_CheckedChanged(radioButton3, null);
+                    button8_Click(null, null);
+                    break;
+                case "Button4":
+                    radioButton4.BeginInvoke(new Action(() => radioButton4.Checked = true));
+                    radioButton1_CheckedChanged(radioButton4, null);
+                    button8_Click(null, null);
+                    break;
+                case "Button5":
+                    radioButton5.BeginInvoke(new Action(() => radioButton5.Checked = true));
+                    radioButton1_CheckedChanged(radioButton5, null);
+                    button8_Click(null, null);
+                    break;
+                case "Button6":
+                    radioButton6.BeginInvoke(new Action(() => radioButton6.Checked = true));
+                    radioButton1_CheckedChanged(radioButton6, null);
+                    button8_Click(null, null);
+                    break;
+            }
+        }
+        SerialPort port;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+                port.WriteLine("p");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+                port.WriteLine("G");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+                port.WriteLine("H");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_lastChoosed)){
+                MessageBox.Show("Сначала нужно выбрать назначаемую клавишу!");
+                return;
+            }
+            if (port.IsOpen)
+            {
+                forWriteBase = null;
+                port.DataReceived -= Port_DataReceived;
+                port.DataReceived += RecieveForRecord;
+                port.WriteLine("R");
+            }
+        }
+
+        private string forWriteBase;
+        private void RecieveForRecord(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(forWriteBase))
+                forWriteBase = _lastChoosed + "|";
+            forWriteBase += port.ReadExisting().Trim()+" ";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+            port.DataReceived -= RecieveForRecord;
+            port.DataReceived += Port_DataReceived;
+            port.WriteLine("Q");
+
+           var allLines = File.ReadAllLines("BASE.txt").ToList();
+            allLines.RemoveAll(f => f.Contains(_lastChoosed + "|"));
+            allLines.Add(forWriteBase);
+            File.WriteAllLines("BASE.txt", allLines);
+
+        }
+
+        private string _lastChoosed;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            _lastChoosed = (sender as RadioButton).Text.Replace("Кнопка ", string.Empty);
+
+        }
+
+        private  async void button8_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen) 
+            {
+                
+                        port.WriteLine("q");
+                        var neededCommand = File.ReadAllLines("BASE.txt").FirstOrDefault(f => f.StartsWith($"{_lastChoosed}|"))?.Split('|').Last().Split(' ');
+                if (neededCommand == null)
+                {
+                    port.WriteLine("228");////
+                    return;
+                }
+                port.WriteLine("001");
+
+                foreach (var f in neededCommand)
+                        {
+                            
+                            port.WriteLine(f);
+                            await Task.Delay(500);//
+                }
+                await Task.Delay(500);//
+                port.WriteLine("228");
+                        
+
+
+            }
+        }
+    }
+}
